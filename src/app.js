360bb855a33654fb2a802400e0bf45ac17203434
Geisniwander Santos <115326392+geisniwander@users.noreply.github.com>
2023-01-30 14:10:38 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
-server.use([authRouter,cartRouter,productsRouter, purchaseRouter]);
+server.use([productsRouter, authRouter,cartRouter, purchaseRouter]);
     
 server.listen(process.env.PORT, () => {
     console.log("Server running on port " + process.env.PORT)
